5524e752125df910b6f8350ad3c0a18daa1e2d55
Mingos <137617609+Myngos@users.noreply.github.com>
2024-10-19 02:04:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
+const express = require('express');
+const mongoose = require('mongoose'); // Corrected the second import
+const path = require('path');
+const app = express(); // Changed `index` to `app` for clarity
+
+
+//configure mongoose to connect to mongodb
+mongoose.connect('mongodb://27017/cms_project', { useNewUrlParser: true})
+     .then(response => {
+        console.log("MongoDB connected successfully.");
+     }).catch(err =>{
+        console.log("Database connection failed!");
+     })
+/*Configure express */
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+/*Routes */
+app.use('/', (req, res)=> {
+    res.send("Welcome the CMS App");
+})
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
