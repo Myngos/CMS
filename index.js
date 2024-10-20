ce5d829dcca5a42a9025f168afd237fbfac460be
Mingos <137617609+Myngos@users.noreply.github.com>
2024-10-20 02:20:26 +0200

    server

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose'); // Corrected the second import
 const path = require('path');
-const app = express(); // Changed `index` to `app` for clarity
+
+const hbs = require('express-handlebars');
+const app = express(); 
 
 
 //configure mongoose to connect to mongodb
@@ -11,18 +13,25 @@ mongoose.connect('mongodb://localhost:27017/cms_project', { useNewUrlParser: tru
      }).catch(err =>{
         console.log("Database connection failed!");
      })
+
+
 /*Configure express */
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+/*SetUp view engine to use handle bars */
+app.engine('handlebars', hbs({defaultLayout: 'default'}));
+app.set('view engine', 'handlebars');
 
 
 /*Routes */
 app.get('/', (req, res) => {
     res.send("Welcome to the CMS App");
 });
+
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
